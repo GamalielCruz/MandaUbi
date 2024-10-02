417850ae3fdfba6fe6bd849d8484e839c8d95d88
Ignacio Cruz <128826239+GamalielCruz@users.noreply.github.com>
2024-10-01 19:24:30 -0600

    Space

--- components/features-planet.tsx
+++ components/features-planet.tsx
@@ -9,9 +9,9 @@ import PlanetTagImg04 from "@/public/images/planet-tag-04.png";
 export default function FeaturesPlanet() {
   return (
     <section className="relative before:absolute before:inset-0 before:-z-20 before:bg-gray-900">
-      <div className="mx-auto max-w-6xl">
+      <div className="mx-auto ">
       <h1
-              className="pt-10 font-bold font-serif  md:text-5xl  text-white text-center"
+              className="pt-10 font-serif  md:text-5xl  text-white text-center"
               data-aos="zoom-y-out"
               data-aos-delay={150}
             >
@@ -22,7 +22,7 @@ export default function FeaturesPlanet() {
           {/* Section header */}
           <div className="mx-auto max-w-3xl">
           <img className="" data-aos="zoom-y-out"
-              data-aos-delay={150} alt="" data-original-height="425" data-original-width="640" src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgVCyNGDf9t6qvW00RHGqCeidDywJ8CllSoxkz8cx00P7hZLKdftOQJ9jUS4GxtsclgvC_nPHweIAH_ROt4OTCR7kT4FeZjkj-Eyp0x6esu-KU0nnGd11nKAZ9c5EyQLRGOXcXro-yI2e1npba-OCUQZQckzeDaEWZ7a3MajEKezkzcbtvYixyUzvkInT0/s1600/Green%20Graphic%20New%20Arrivals%20Website%20Homepage%20Banner%20%284%29.png"/>          
+              data-aos-delay={150} alt="" data-original-height="425" data-original-width="640" src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEhDkow4r54A9BJRFTRdNlwq5WqO1cp3mbLeu7sgOBxeljvTNsY1ydsoSHSjS2Q5Pp5g_Z9E1t2IzsborifFc18S9r8zd-0BnTx8kjPrD84sP8PkZ2iaQHaJzH6OpxJh5YnkKtbgTEg9u5ax9G9Y6fCGa7lcXbISkh-wRKlo-eLqtjoC0V6Yu45NxTzC3YE/s1600/Green%20Graphic%20New%20Arrivals%20Website%20Homepage%20Banner%20%28900%20x%20425%20px%29.png"/>          
           </div>
         </div>
       </div>
