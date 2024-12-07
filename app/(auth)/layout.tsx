4691014614f178b76c59f4082741cd718070978d
Ignacio Cruz <128826239+GamalielCruz@users.noreply.github.com>
2024-12-06 18:08:33 -0600

    Add 2nd seccion and passes

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -5,8 +5,9 @@ export default function layout() {
   return (
     <section className="relative">
       <script src="https://cdn.jsdelivr.net/npm/add-to-calendar-button@2" async defer></script>
-            <PageIllustrationOne/>
-            <div className="w-full h-screen max-h-[36rem] p-24 relative overflow-hidden block z-10
+            
+            <div>
+            <div className="w-full h-screen max-h-[42rem] p-24 overflow-hidden block z-10
             bg-[url('https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEiVqXZqORModfP26L_AuuqsXCZLKJMgLMjqxkYd_KoIsY7HTSiaYGMVH7Gi-yyh0zXLtwab1tK3os1TTXX_notw7pqEOtPWzHD9c_7BPksZk0QTEkUAWr0KxEiewR9NYoqKaGwmJbLCR0Qync65rVTU4RmzEjuHrZN4-bxzG7Wi1oUHSs__QOaKjSwQsKQ/s1600/Dise%C3%B1o%20sin%20t%C3%ADtulo%20%285%29.png')]
             bg-cover
             bg-no-repeat
@@ -54,12 +55,63 @@ export default function layout() {
               iCalFileName="Reminder-Event"
               label="Agregar a mi calendario"
               />
+            </div>           
+            </div>
+            
+            </div>
+            
+            {/*Nubes*/}
+            <div className="relative -mt-12 lg:-mt-24">
+            <svg viewBox="0 0 1428 174" version="1.1" xmlns="http://www.w3.org/2000/svg" >
+        <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+          <g transform="translate(-2.000000, 44.000000)" fill="#FFFFFF" fill-rule="nonzero">
+            <path d="M0,0 C90.7283404,0.927527913 147.912752,27.187927 291.910178,59.9119003 C387.908462,81.7278826 543.605069,89.334785 759,82.7326078 C469.336065,156.254352 216.336065,153.6679 0,74.9732496" opacity="0.100000001"></path>
+            <path
+              d="M100,104.708498 C277.413333,72.2345949 426.147877,52.5246657 546.203633,45.5787101 C666.259389,38.6327546 810.524845,41.7979068 979,55.0741668 C931.069965,56.122511 810.303266,74.8455141 616.699903,111.243176 C423.096539,147.640838 250.863238,145.462612 100,104.708498 Z"
+              opacity="0.100000001"
+            ></path>
+            <path d="M1046,51.6521276 C1130.83045,29.328812 1279.08318,17.607883 1439,40.1656806 L1439,120 C1271.17211,77.9435312 1140.17211,55.1609071 1046,51.6521276 Z" id="Path-4" opacity="0.200000003"></path>
+          </g>
+          <g transform="translate(-4.000000, 76.000000)" fill="#FFFFFF" fill-rule="nonzero">
+            <path
+              d="M0.457,34.035 C57.086,53.198 98.208,65.809 123.822,71.865 C181.454,85.495 234.295,90.29 272.033,93.459 C311.355,96.759 396.635,95.801 461.025,91.663 C486.76,90.01 518.727,86.372 556.926,80.752 C595.747,74.596 622.372,70.008 636.799,66.991 C663.913,61.324 712.501,49.503 727.605,46.128 C780.47,34.317 818.839,22.532 856.324,15.904 C922.689,4.169 955.676,2.522 1011.185,0.432 C1060.705,1.477 1097.39,3.129 1121.236,5.387 C1161.703,9.219 1208.621,17.821 1235.4,22.304 C1285.855,30.748 1354.351,47.432 1440.886,72.354 L1441.191,104.352 L1.121,104.031 L0.457,34.035 Z"
+            ></path>
+          </g>
+        </g>
+            </svg>
+            </div>
+            
+            
+            {/*2 Nombre del invitado*/}
+            <div className="p-10 m-0 bg-white">              
+            <div className="relative block justify-items-center">          
+	          <h1 className="p-10 text-gray-600 text-center font-serif font-extralight text-3xl justify-center">Invitación para:
+            </h1>
+            <h1 className="p-6 text-4xl text-gray-950 font-serif font-semibold flex justify-center">
+                Nombre del invitado
+            </h1>            
+            <PageIllustrationOne/>
+            <h1 className="p-10 text-gray-900 font-serif">¡Estaría encantada de que me acompañaras en este día tan especial!</h1>          
+            </div>
+            </div>
+            
+            <div className=" block justify-items-center bg-white">
+            <h3 className="mb-2 flex items-center space-x-2 font-medium">
+              <img
+                  className=""
+                  width={60}
+                  height={60}
+                  src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEhivapmh1_caSrvpXw-AT_1CAv1F1wjTVSeUZhfEEHXXFg8w0XYhzyPObP9GCq6fa29EeC_y0tiw9Hrq9QBU1Vdyd_aJxFk0EEZaiLTxx3I21TatLZkW5gus4bqwGW3XfZq_La_eZRWceGq7HDHVrv2SiYd0dSfzso86Q7I2Himv7mLlOanFb5j-7x1CIQ/s1600/a%20%285%29.png"
+                /> 
+                <div className="text-5xl font-serif">3</div>
+              </h3>
+              <p className=" text-gray-900 text-2xl font-serif">Pases disponibles</p>
+            </div>
             </div>
 
-            </div>
+            <div className="p-20">
 
             </div>
-               
 
     </section>
   );
