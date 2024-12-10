1a285b95404d3399eb319baafbb2f60e580b9f06
Ignacio Cruz <128826239+GamalielCruz@users.noreply.github.com>
2024-12-10 17:38:01 -0600

    v9 PC

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -131,15 +131,16 @@ export default function layout() {
              className=" rounded-md md:rounded-lg"
           />
            </div>
-          <div className="p-6 inline-block justify-items-center">
+          <div className="p-6 static justify-items-center">
           <div className="absolute inset-0 -z-10 h-full w-full bg-yellow-50 bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]"><div className="absolute left-0 right-0 top-0 -z-10 m-auto h-[310px] w-[310px] rounded-full bg-yellow-600 opacity-30 blur-[100px]"></div></div>          
-          <div className="w-4/12 justify-items-center m-5">
-          <img
-             src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgnwfeO-m-svA1AuRZ4jwrUf1YbU1qoJg10yXMftEJpY1cgyULTujSizv9i-ijYx06fTNhAGRAbmYmwzxKmb4UVBXhY5U69VOIEDDToAREwRTf2y_YWyRrBSuWaU4iGSS4m6oO2gllLZiLvDOWLho82AT52doQ06EYh1lxqVUrsxENdPDLHdWkmnsPcDtQ/s1600/PADRES%20%281%29.png"
-             alt="card-image"
-             className="  justify-items-center"
-          />
-          </div>
+          <h3 className="  font-medium text-5xl font-serif">
+              <img
+                  className="inline-block justify-items-center"
+                  width={200}
+                  height={160}
+                  src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgnwfeO-m-svA1AuRZ4jwrUf1YbU1qoJg10yXMftEJpY1cgyULTujSizv9i-ijYx06fTNhAGRAbmYmwzxKmb4UVBXhY5U69VOIEDDToAREwRTf2y_YWyRrBSuWaU4iGSS4m6oO2gllLZiLvDOWLho82AT52doQ06EYh1lxqVUrsxENdPDLHdWkmnsPcDtQ/s1600/PADRES%20%281%29.png"
+                />
+              </h3>
           <h4 className="mb-2 text-gray-900 text-4xl text-center font-serif">
           Fernando Gutiérrez 
           </h4>
