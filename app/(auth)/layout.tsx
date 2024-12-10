652debf53e0a8aa460b2f353009ef637a22b165d
Ignacio Cruz <128826239+GamalielCruz@users.noreply.github.com>
2024-12-10 17:12:24 -0600

    v6

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -131,7 +131,7 @@ export default function layout() {
              className=" rounded-md md:rounded-lg"
           />
            </div>
-          <div className="p-6 relative justify-items-center">
+          <div className="p-6 justify-items-center">
           <div className="absolute inset-0 -z-10 h-full w-full bg-yellow-50 bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]"><div className="absolute left-0 right-0 top-0 -z-10 m-auto h-[310px] w-[310px] rounded-full bg-yellow-600 opacity-30 blur-[100px]"></div></div>          
           <div className="relative justify-items-center">
           <img
