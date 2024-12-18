1fd76c3adc64e555378a092afa9e8839860fecb3
Ignacio Cruz <128826239+GamalielCruz@users.noreply.github.com>
2024-12-18 17:27:17 -0600

    v1 Confirmación

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -224,6 +224,10 @@ export default function layout() {
 </div>
 
     </div>
+    {/* Separador */}  
+    <div>
+    <div className="bg-yellow-950 text-transparent">-</div>
+    </div>
     {/* Mesa de regalos */}    
     <div className="relative">
       
@@ -292,6 +296,10 @@ export default function layout() {
 </div>
 
     </div>
+    {/* Separador */}  
+    <div>
+    <div className="bg-yellow-950 text-transparent">-</div>
+    </div>
     {/* Información familiar mas fotos */}       
     <div className="flex flex-col items-center  p-5">
 
@@ -357,6 +365,14 @@ export default function layout() {
   </div>
 </div>
     </div>
+    {/* Separador */}      
+    <div className=" flex justify-center h-40 ">
+           <img
+           className=" "
+           src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEhPyW2-8XtYLKVfjQlHJE7QJOwn7ce_hSh7O0l_UIsmkU9xMzbrlCTmEBN-L_mZrlzI6MPgDSxgtEQzvzVt-owCVFGdDf3L3G83ohj_qRKlWaK5L0W3VLbxOnDMWfcujtl6pF7CdQDrTaHa4Hevoa1eDxg2meNlnWvxBNNgcgFaEFYStoV6NTNuOgnI9bo/s1600/Dise%C3%B1o%20sin%20t%C3%ADtulo%20%2812%29.png"
+           alt="Imagen de separador vertical"
+            />
+    </div>
     {/* Confirmación de asistencia */}    
     <div className=" justify-items-center">
     <div className="flex justify-center items-center">
@@ -370,105 +386,63 @@ export default function layout() {
     
     <div className="flex flex-wrap justify-center gap-5 p-4">
     <div className="flex flex-col shadow-lg border w-full sm:w-80 max-w-sm">
-    <div className=" justify-items-center p-5">
-    <svg fill="#B7B4A8" 
-        xmlns="http://www.w3.org/2000/svg" 
-        version="1.1" 
-        id="Layer_1" 
-        viewBox="0 0 400 400" 
-        preserveAspectRatio="xMidYMid meet" 
-        style={{ height: "50px", width: "50px" }}
-    >
-<g id="XMLID_468_">
-	<path id="XMLID_469_" d="M227.904,176.981c-0.6-0.288-23.054-11.345-27.044-12.781c-1.629-0.585-3.374-1.156-5.23-1.156
-		c-3.032,0-5.579,1.511-7.563,4.479c-2.243,3.334-9.033,11.271-11.131,13.642c-0.274,0.313-0.648,0.687-0.872,0.687
-		c-0.201,0-3.676-1.431-4.728-1.888c-24.087-10.463-42.37-35.624-44.877-39.867c-0.358-0.61-0.373-0.887-0.376-0.887
-		c0.088-0.323,0.898-1.135,1.316-1.554c1.223-1.21,2.548-2.805,3.83-4.348c0.607-0.731,1.215-1.463,1.812-2.153
-		c1.86-2.164,2.688-3.844,3.648-5.79l0.503-1.011c2.344-4.657,0.342-8.587-0.305-9.856c-0.531-1.062-10.012-23.944-11.02-26.348
-		c-2.424-5.801-5.627-8.502-10.078-8.502c-0.413,0,0,0-1.732,0.073c-2.109,0.089-13.594,1.601-18.672,4.802
-		c-5.385,3.395-14.495,14.217-14.495,33.249c0,17.129,10.87,33.302,15.537,39.453c0.116,0.155,0.329,0.47,0.638,0.922
-		c17.873,26.102,40.154,45.446,62.741,54.469c21.745,8.686,32.042,9.69,37.896,9.69c0.001,0,0.001,0,0.001,0
-		c2.46,0,4.429-0.193,6.166-0.364l1.102-0.105c7.512-0.666,24.02-9.22,27.775-19.655c2.958-8.219,3.738-17.199,1.77-20.458
-		C233.168,179.508,230.845,178.393,227.904,176.981z"/>
-	<path id="XMLID_470_" d="M156.734,0C73.318,0,5.454,67.354,5.454,150.143c0,26.777,7.166,52.988,20.741,75.928L0.212,302.716
-		c-0.484,1.429-0.124,3.009,0.933,4.085C1.908,307.58,2.943,308,4,308c0.405,0,0.813-0.061,1.211-0.188l79.92-25.396
-		c21.87,11.685,46.588,17.853,71.604,17.853C240.143,300.27,308,232.923,308,150.143C308,67.354,240.143,0,156.734,0z
-		 M156.734,268.994c-23.539,0-46.338-6.797-65.936-19.657c-0.659-0.433-1.424-0.655-2.194-0.655c-0.407,0-0.815,0.062-1.212,0.188
-		l-40.035,12.726l12.924-38.129c0.418-1.234,0.209-2.595-0.561-3.647c-14.924-20.392-22.813-44.485-22.813-69.677
-		c0-65.543,53.754-118.867,119.826-118.867c66.064,0,119.812,53.324,119.812,118.867
-		C276.546,215.678,222.799,268.994,156.734,268.994z"/>
-</g>
-    </svg>  
-    </div>  
-    <h4 className="p-2 m-3 text-2xl font-serif text-slate-900 text-center ">
-    Por WhatsApp
-    </h4>
-    <div className="flex justify-center p-4 gap-2">
-      <a
-        href="https://www.whatsapp.com"
-        className="text-white bg-[#322c08a0] hover:bg-[#322c08]/90 focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Con Brenda
-      </a>
+        <div className="flex justify-center items-center p-5">
+            <svg fill="#B7B4A8" xmlns="http://www.w3.org/2000/svg" version="1.1" id="Layer_1" viewBox="0 0 400 400" preserveAspectRatio="xMidYMid meet" style={{ height: "50px", width: "50px" }}>
+                <g id="XMLID_468_">
+                    <path id="XMLID_469_" d="M227.904,176.981c-0.6-0.288-23.054-11.345-27.044-12.781c-1.629-0.585-3.374-1.156-5.23-1.156
+                    c-3.032,0-5.579,1.511-7.563,4.479c-2.243,3.334-9.033,11.271-11.131,13.642c-0.274,0.313-0.648,0.687-0.872,0.687
+                    c-0.201,0-3.676-1.431-4.728-1.888c-24.087-10.463-42.37-35.624-44.877-39.867c-0.358-0.61-0.373-0.887-0.376-0.887
+                    c0.088-0.323,0.898-1.135,1.316-1.554c1.223-1.21,2.548-2.805,3.83-4.348c0.607-0.731,1.215-1.463,1.812-2.153
+                    c1.86-2.164,2.688-3.844,3.648-5.79l0.503-1.011c2.344-4.657,0.342-8.587-0.305-9.856c-0.531-1.062-10.012-23.944-11.02-26.348
+                    c-2.424-5.801-5.627-8.502-10.078-8.502c-0.413,0,0,0-1.732,0.073c-2.109,0.089-13.594,1.601-18.672,4.802
+                    c-5.385,3.395-14.495,14.217-14.495,33.249c0,17.129,10.87,33.302,15.537,39.453c0.116,0.155,0.329,0.47,0.638,0.922
+                    c17.873,26.102,40.154,45.446,62.741,54.469c21.745,8.686,32.042,9.69,37.896,9.69c0.001,0,0.001,0,0.001,0
+                    c2.46,0,4.429-0.193,6.166-0.364l1.102-0.105c7.512-0.666,24.02-9.22,27.775-19.655c2.958-8.219,3.738-17.199,1.77-20.458
+                    C233.168,179.508,230.845,178.393,227.904,176.981z"/>
+                    <path id="XMLID_470_" d="M156.734,0C73.318,0,5.454,67.354,5.454,150.143c0,26.777,7.166,52.988,20.741,75.928L0.212,302.716
+                    c-0.484,1.429-0.124,3.009,0.933,4.085C1.908,307.58,2.943,308,4,308c0.405,0,0.813-0.061,1.211-0.188l79.92-25.396
+                    c21.87,11.685,46.588,17.853,71.604,17.853C240.143,300.27,308,232.923,308,150.143C308,67.354,240.143,0,156.734,0z
+                     M156.734,268.994c-23.539,0-46.338-6.797-65.936-19.657c-0.659-0.433-1.424-0.655-2.194-0.655c-0.407,0-0.815,0.062-1.212,0.188
+                    l-40.035,12.726l12.924-38.129c0.418-1.234,0.209-2.595-0.561-3.647c-14.924-20.392-22.813-44.485-22.813-69.677
+                    c0-65.543,53.754-118.867,119.826-118.867c66.064,0,119.812,53.324,119.812,118.867
+                    C276.546,215.678,222.799,268.994,156.734,268.994z"/>
+                </g>
+            </svg>
+        </div>  
+        <h4 className="p-2 m-3 text-2xl font-serif text-slate-900 text-center">
+            Por WhatsApp
+        </h4>
+        <div className="flex justify-center p-4 gap-2">
+            <a href="https://www.whatsapp.com" className="text-white bg-[#322c08a0] hover:bg-[#322c08]/90 focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center" target="_blank" rel="noopener noreferrer">
+                Con Brenda
+            </a>
+        </div>
+        <div className="flex justify-center p-4 gap-2">
+            <a href="https://www.whatsapp.com" className="text-white bg-[#322c08a0] hover:bg-[#322c08]/90 focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center" target="_blank" rel="noopener noreferrer">
+                Con Fernado
+            </a>
+        </div>
     </div>
-    <div className="flex justify-center p-4 gap-2">
-      <a
-        href="https://www.whatsapp.com"
-        className="text-white bg-[#322c08a0] hover:bg-[#322c08]/90 focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Con Fernado
-      </a>
-    </div>
-    </div>
+
     <div className="flex flex-col shadow-lg border w-full sm:w-80 max-w-sm">
-    <div className=" justify-items-center p-5">
-    <div style={{ width: "100%", maxWidth: "50px" }}>
-    <svg 
-        xmlns="http://www.w3.org/2000/svg" 
-        viewBox="0 0 24 24" 
-        preserveAspectRatio="xMidYMid meet" 
-        style={{ width: "100%", height: "auto" }}
-    >
-        <path 
-            fill-rule="evenodd" 
-            clip-rule="evenodd" 
-            d="M17.3545 22.2323C15.3344 21.7262 11.1989 20.2993 7.44976 16.5502C3.70065 12.8011 2.2738 8.66559 1.76767 6.6455C1.47681 5.48459 2.00058 4.36434 2.88869 3.72997L5.21694 2.06693C6.57922 1.09388 8.47432 1.42407 9.42724 2.80051L10.893 4.91776C11.5152 5.8165 11.3006 7.0483 10.4111 7.68365L9.24234 8.51849C9.41923 9.1951 9.96939 10.5846 11.6924 12.3076C13.4154 14.0306 14.8049 14.5807 15.4815 14.7576L16.3163 13.5888C16.9517 12.6994 18.1835 12.4847 19.0822 13.1069L21.1995 14.5727C22.5759 15.5257 22.9061 17.4207 21.933 18.783L20.27 21.1113C19.6356 21.9994 18.5154 22.5232 17.3545 22.2323ZM8.86397 15.136C12.2734 18.5454 16.0358 19.8401 17.8405 20.2923C18.1043 20.3583 18.4232 20.2558 18.6425 19.9488L20.3056 17.6205C20.6299 17.1665 20.5199 16.5348 20.061 16.2171L17.9438 14.7513L17.0479 16.0056C16.6818 16.5182 16.0047 16.9202 15.2163 16.7501C14.2323 16.5378 12.4133 15.8569 10.2782 13.7218C8.1431 11.5867 7.46219 9.7677 7.24987 8.7837C7.07977 7.9953 7.48181 7.31821 7.99439 6.95208L9.24864 6.05618L7.78285 3.93893C7.46521 3.48011 6.83351 3.37005 6.37942 3.6944L4.05117 5.35744C3.74413 5.57675 3.64162 5.89565 3.70771 6.15943C4.15989 7.96418 5.45459 11.7266 8.86397 15.136Z" 
-            fill="#B7B4A8"
-        />
-    </svg>
+        <div className="flex justify-center items-center p-5">
+            <div style={{ width: "100%", maxWidth: "50px" }}>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" preserveAspectRatio="xMidYMid meet" style={{ width: "100%", height: "auto" }}>
+                <path fill-rule="evenodd" clip-rule="evenodd" d="M17.3545 22.2323C15.3344 21.7262 11.1989 20.2993 7.44976 16.5502C3.70065 12.8011 2.2738 8.66559 1.76767 6.6455C1.47681 5.48459 2.00058 4.36434 2.88869 3.72997L5.21694 2.06693C6.57922 1.09388 8.47432 1.42407 9.42724 2.80051L10.893 4.91776C11.5152 5.8165 11.3006 7.0483 10.4111 7.68365L9.24234 8.51849C9.41923 9.1951 9.96939 10.5846 11.6924 12.3076C13.4154 14.0306 14.8049 14.5807 15.4815 14.7576L16.3163 13.5888C16.9517 12.6994 18.1835 12.4847 19.0822 13.1069L21.1995 14.5727C22.5759 15.5257 22.9061 17.4207 21.933 18.783L20.27 21.1113C19.6356 21.9994 18.5154 22.5232 17.3545 22.2323ZM8.86397 15.136C12.2734 18.5454 16.0358 19.8401 17.8405 20.2923C18.1043 20.3583 18.4232 20.2558 18.6425 19.9488L20.3056 17.6205C20.6299 17.1665 20.5199 16.5348 20.061 16.2171L17.9438 14.7513L17.0479 16.0056C16.6818 16.5182 16.0047 16.9202 15.2163 16.7501C14.2323 16.5378 12.4133 15.8569 10.2782 13.7218C8.1431 11.5867 7.46219 9.7677 7.24987 8.7837C7.07977 7.9953 7.48181 7.31821 7.99439 6.95208L9.24864 6.05618L7.78285 3.93893C7.46521 3.48011 6.83351 3.37005 6.37942 3.6944L4.05117 5.35744C3.74413 5.57675 3.64162 5.89565 3.70771 6.15943C4.15989 7.96418 5.45459 11.7266 8.86397 15.136Z" fill="#B7B4A8"/>
+                </svg>
+            </div>
+        </div>
+        <h4 className="p-2 m-3 text-2xl font-serif text-slate-900 text-center">
+            Por Teléfono
+        </h4>
+        <div className="flex justify-center p-4 gap-2">
+            <a href="tel:+123456789" className="text-white bg-[#322c08a0] hover:bg-[#322c08]/90 focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center">
+                Llamar
+            </a>
+        </div>
+    </div>
 </div>
 
-	
-    </div>  
-    <h4 className="p-2 m-3 text-2xl font-serif text-slate-900 text-center ">
-    Por llamada
-    </h4>
-    <div className="flex justify-center p-4 gap-2">
-      <a
-        href="https://www.whatsapp.com"
-        className="text-white bg-[#322c08a0] hover:bg-[#322c08]/90 focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Con Brenda
-      </a>
-    </div>
-    <div className="flex justify-center p-4 gap-2">
-      <a
-        href="https://www.whatsapp.com"
-        className="text-white bg-[#322c08a0] hover:bg-[#322c08]/90 focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Con Fernado
-      </a>
-    </div>
-    </div>
-    </div>
     
     </div>
 
