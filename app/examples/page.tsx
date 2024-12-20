0a19df73e2cf481c263deb44b97255837d636d0f
Ignacio Cruz <128826239+GamalielCruz@users.noreply.github.com>
2024-12-19 21:42:17 -0600

    New feutures added modelos IslaCoral

--- app/examples/page.tsx
+++ app/examples/page.tsx
@@ -1,14 +1,49 @@
-import 'add-to-calendar-button';
-import Link from "next/link";
+"use client";
+
+import SilderImg01 from '@/public/images/avatar-01.png'
+import SilderImg02 from '@/public/images/avatar-02.jpg'
+import SilderImg03 from '@/public/images/avatar-03.jpg'
+import SilderImg04 from '@/public/images/avatar-04.jpg'
+import SilderIcon01 from '@/public/images/logo-01.svg'
+import SilderIcon02 from '@/public/images/logo-02.svg'
+import SilderIcon03 from '@/public/images/logo-03.svg'
+import SilderIcon04 from '@/public/images/logo-04.svg'
 
 import Logo from "@/components/ui/logo";
+import Image from 'next/image';
+import { useState } from 'react';
 
 
-export default function Examples() {
+export default function ProgressSlider() {
+
+  const items = [
+    {
+      img: SilderImg01,
+      desc: 'XV Años',
+      buttonIcon: SilderIcon01,
+    },
+    {
+      img: SilderImg02,
+      desc: 'Bodas',
+      buttonIcon: SilderIcon02,
+    },
+    {
+      img: SilderImg03,
+      desc: 'Fiestas',
+      buttonIcon: SilderIcon03,
+    },
+    {
+      img: SilderImg04,
+      desc: 'Baby Shower',
+      buttonIcon: SilderIcon04,
+  }
+  ]; 
+
+  const [currentIndex, setCurrentIndex] = useState(0);
+
   return (
     <>
     <>
-   
     <header className="absolute z-30 w-full">
         <div className="mx-auto max-w-6xl px-4 sm:px-6">
           <div className="flex h-16 items-center justify-between md:h-20">
@@ -20,46 +55,59 @@ export default function Examples() {
         </div>
       </header>
 
-      <main className="p-5">
+      <main className="">
+        <div className='static mt-20'>
+        <h2 className="mb-1 border-y text-2xl font-bold text-center text-sky-950 [border-image:linear-gradient(to_right,transparent,theme(colors.slate.400/.7),transparent)1] md:text-4xl">
+            Modelos
+        </h2>
+        </div>
 
-        <div>
-        <div className="px-4 py-12 md:px-12 md:py-20">
-            <h2 className="mb-1 border-y text-2xl font-bold text-center text-gray-900 [border-image:linear-gradient(to_right,transparent,theme(colors.slate.400/.7),transparent)1] md:text-4xl">
-            Modelos</h2>
-
-            <div className="mx-auto max-w-xs sm:flex sm:max-w-none sm:justify-center">
-             
-            <div className="relative px-1 py-2 sm:py-1 lg:px-1s">
-
-  <div className="absolute inset-x-0 -top-3 -z-10 transform-gpu overflow-hidden px-36 blur-3xl" aria-hidden="true">
-    <div className="mx-auto aspect-[1155/678] w-[72.1875rem] bg-gradient-to-tr " ></div>
-  </div>
-  
-  <div className="mx-auto mt-16 grid max-w-lg grid-cols-1 items-center gap-y-6  sm:gap-y-0 lg:max-w-3xl lg:grid-cols-2">
-    <div className="rounded-3xl rounded-t-3xl bg-sky-100 p-8 ring-1 ring-gray-900/10 sm:mx-8   lg:mx-0 lg:rounded-bl-3xl ">
-      <h3 id="tier-hobby" className="text-base/7 font-semibold text-sky-700">Coral</h3>
-      
-      
-      <div className="p-5 flex justify-center ">
-      <img
-        className="w-40"  
-        src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEiikaM5ooBiH9TPnFomTHKxKv4il5Ih70a-2d2jA5qySfqwAwp-Vadahy_6uSYEBBdOJxn880XQ1LG8-knJvdzsrsbUQicia52rmzzoEqktnaNJIK-HEIXal_WnjEWCzMJuLahJM1JZAKIY0GbtOsySWBmiTULnSqKRMBnE130GfgyK3_AiTYYX8VRS6Ww/s1600/Sin%20t%C3%ADtulo%20%28410%20x%20844%20px%29.png"
-        alt="islacoral"
-      />
-    </div>
-    <p className="mt-6 text-base/7 text-gray-800">Ideal para XV años</p>
-      <Link href="/islacoral" aria-describedby="tier-hobby" className="mt-8 block rounded-md px-3.5 py-2.5 text-center text-sm font-semibold text-indigo-600 ring-1 ring-inset ring-indigo-200 hover:ring-indigo-300 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 sm:mt-10">Ver completo</Link>
-    </div>
-  </div>
-</div>
-
-              
-              
-            </div>
-         
+        <div className="max-w-xl mx-auto  ">
+      <div className="w-full  mx-auto text-center ">
+        {/* Slider container */}
+        <div className="relative overflow-hidden ">
+          <div className="flex transition-transform duration-200 ease-in-out" style={{ transform: `translateX(-${currentIndex * 100}%)` }}>
+        {items.map((item, index) => (
+          <div key={index} className="flex-shrink-0 w-full">
+            <Image className="" src={item.img} width={524} height={206} alt={item.desc} />
+          </div>
+        ))}
           </div>
         </div>
-        
+        {/* Clickable images */}
+        <div className="absolute inset-0 flex items-center justify-center">
+          {items.map((item, index) => (
+            <a
+              key={index}
+              href={`/isla/${index}`}
+              className={` w-full  ${currentIndex === index ? 'block' : 'hidden'}`}
+            >
+              <span className="sr-only">{item.desc}</span>
+            </a>
+          ))}
+        </div>
+        {/* Buttons */}
+        <div className="max-w-xs sm:max-w-sm md:max-w-3xl mx-auto grid grid-cols-2 md:grid-cols-4 gap-4 mt-8">
+          {items.map((item, index) => (
+        <button
+          key={index}
+          className="p-2 rounded focus:outline-none focus-visible:ring focus-visible:ring-indigo-300 group"
+          onClick={() => setCurrentIndex(index)}
+        >
+          <span className="text-center flex flex-col items-center shadow-xl bg-gray-100 p-3">
+            <span className="flex items-center justify-center relative w-9 h-9 rounded-full bg-gray-200 mb-2">
+          <Image src={item.buttonIcon} alt={item.desc} />
+            </span>
+            <span className="block text-md font-medium text-slate-900 mb-2">{item.desc}</span>
+            <span className="block relative w-full bg-slate-200 h-1 rounded-full" role="progressbar" aria-valuenow={currentIndex === index ? 100 : 0}>
+          <span className="absolute inset-0 bg-gray-700 rounded-[inherit]" style={{ width: currentIndex === index ? '100%' : '0%' }}></span>
+            </span>
+          </span>
+        </button>
+          ))}
+        </div>
+      </div>
+      </div>
       </main>
     </>
     </>
