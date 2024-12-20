f91abc2ddde2483979afd839069ecfb92e58c722
Ignacio Cruz <128826239+GamalielCruz@users.noreply.github.com>
2024-12-20 17:41:01 -0600

    Updated with new page

--- app/examples/page.tsx
+++ app/examples/page.tsx
@@ -1,53 +1,39 @@
 "use client";
 
-import SilderImg01 from '@/public/images/avatar-01.png'
-import SilderImg02 from '@/public/images/avatar-02.jpg'
-import SilderImg03 from '@/public/images/avatar-03.jpg'
-import SilderImg04 from '@/public/images/avatar-04.jpg'
-import SilderIcon01 from '@/public/images/logo-01.svg'
-import SilderIcon02 from '@/public/images/logo-02.svg'
-import SilderIcon03 from '@/public/images/logo-03.svg'
-import SilderIcon04 from '@/public/images/logo-04.svg'
-
 import Logo from "@/components/ui/logo";
 import Image from 'next/image';
+import Link from 'next/link';
 import { useState } from 'react';
 
+const islands = [
+  {
+    name: 'Isla Coral',
+    image: '/images/isla-coral.png',
+    description: 'Ideal para XV años, evetos de empresa y más. Conoce más de Isla Coral.',
+    link: '/isla/0'
+  },
+  {
+    name: 'Isla del Sol',
+    image: '/images/avatar-01.png',
+    description: 'Descripción de Isla del Sol',
+    link: '/isla-del-sol'
+  },
+  {
+    name: 'Isla Esmeralda',
+    image: '/images/avatar-02.jpg',
+    description: 'Descripción de Isla Esmeralda',
+    link: '/isla-esmeralda'
+  }
+];
 
 export default function ProgressSlider() {
-
-  const items = [
-    {
-      img: SilderImg01,
-      desc: 'XV Años',
-      buttonIcon: SilderIcon01,
-    },
-    {
-      img: SilderImg02,
-      desc: 'Bodas',
-      buttonIcon: SilderIcon02,
-    },
-    {
-      img: SilderImg03,
-      desc: 'Fiestas',
-      buttonIcon: SilderIcon03,
-    },
-    {
-      img: SilderImg04,
-      desc: 'Baby Shower',
-      buttonIcon: SilderIcon04,
-  }
-  ]; 
-
-  const [currentIndex, setCurrentIndex] = useState(0);
+  const [selectedIsland, setSelectedIsland] = useState(islands[0]);
 
   return (
     <>
-    <>
-    <header className="absolute z-30 w-full">
+      <header className="relative w-full">
         <div className="mx-auto max-w-6xl px-4 sm:px-6">
           <div className="flex h-16 items-center justify-between md:h-20">
-            {/* Site branding */}
             <div className="mr-4 shrink-0">
               <Logo />
             </div>
@@ -55,61 +41,29 @@ export default function ProgressSlider() {
         </div>
       </header>
 
-      <main className="">
-        <div className='static mt-20'>
-        <h2 className="mb-1 border-y text-2xl font-bold text-center text-sky-950 [border-image:linear-gradient(to_right,transparent,theme(colors.slate.400/.7),transparent)1] md:text-4xl">
-            Modelos
-        </h2>
-        </div>
-
-        <div className="max-w-xl mx-auto  ">
-      <div className="w-full  mx-auto text-center ">
-        {/* Slider container */}
-        <div className="relative overflow-hidden ">
-          <div className="flex transition-transform duration-200 ease-in-out" style={{ transform: `translateX(-${currentIndex * 100}%)` }}>
-        {items.map((item, index) => (
-          <div key={index} className="flex-shrink-0 w-full">
-            <Image className="" src={item.img} width={524} height={206} alt={item.desc} />
-          </div>
+      <div className="inline-flex rounded-md shadow-sm place-content-center" role="group">
+        {islands.map((island, index) => (
+          <button
+            key={index}
+            type="button"
+            className={`px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 ${
+              index === 0 ? 'rounded-s-lg' : index === islands.length - 1 ? 'rounded-e-lg' : ''
+            } hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-800 dark:border-gray-700 dark:text-white dark:hover:text-white dark:hover:bg-gray-700 dark:focus:ring-blue-500 dark:focus:text-white`}
+            onClick={() => setSelectedIsland(island)}
+          >
+            {island.name}
+          </button>
         ))}
-          </div>
-        </div>
-        {/* Clickable images */}
-        <div className="absolute inset-0 flex items-center justify-center">
-          {items.map((item, index) => (
-            <a
-              key={index}
-              href={`/isla/${index}`}
-              className={`absolute w-full h-full ${currentIndex === index ? 'block' : 'hidden'}`}
-            >
-              <span className="sr-only">{item.desc}</span>
-            </a>
-          ))}
-        </div>
-        {/* Buttons */}
-        <div className="max-w-xs sm:max-w-sm md:max-w-3xl mx-auto grid grid-cols-2 md:grid-cols-4 gap-4 mt-8">
-          {items.map((item, index) => (
-        <button
-          key={index}
-          className="p-2 rounded focus:outline-none focus-visible:ring focus-visible:ring-indigo-300 group"
-          onClick={() => setCurrentIndex(index)}
-        >
-          <span className="text-center flex flex-col items-center shadow-xl bg-gray-100 p-3">
-            <span className="flex items-center justify-center relative w-9 h-9 rounded-full bg-gray-200 mb-2">
-          <Image src={item.buttonIcon} alt={item.desc} />
-            </span>
-            <span className="block text-md font-medium text-slate-900 mb-2">{item.desc}</span>
-            <span className="block relative w-full bg-slate-200 h-1 rounded-full" role="progressbar" aria-valuenow={currentIndex === index ? 100 : 0}>
-          <span className="absolute inset-0 bg-gray-700 rounded-[inherit]" style={{ width: currentIndex === index ? '100%' : '0%' }}></span>
-            </span>
-          </span>
-        </button>
-          ))}
-        </div>
       </div>
+
+      <div className="mt-4 p-4 border rounded-lg shadow-lg place-items-center">
+        <h2 className="text-3xl mb-2 font-serif">{selectedIsland.name}</h2>
+        <Image src={selectedIsland.image} alt={selectedIsland.name} width={200} height={300} className="mb-4" priority />
+        <p className="mb-4">{selectedIsland.description}</p>
+        <Link href={selectedIsland.link} className="px-4 py-2 text-sky-50 bg-stone-800 rounded-lg shadow-xl  hover:bg-blue-700">
+          Pantalla Completa
+        </Link>
       </div>
-      </main>
-    </>
     </>
   );
 }
